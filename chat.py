49ce0da0da6b3e9b0edd83d82b719007440675f0
bellabula <zz225864@gmail.com>
2022-01-28 13:44:02 +0800

    fist commit

---
+++ chat.py
@@ -0,0 +1,34 @@
+# 對話紀錄
+
+# 讀取檔案
+def read_file(filename):
+    data = []
+    with open(filename, 'r', encoding = 'utf-8-sig') as f:
+        for line in f:
+            data.append(line.strip())
+    return data
+
+# 改寫對話紀錄形式
+def convert(data, person):
+    chat = []
+    for c in data:
+        if c in person:
+            name = c
+            continue
+        chat.append(name + ': ' + c)
+    return chat
+
+# 寫入檔案
+def write_file(filename, data):
+    with open('output.txt', 'w', encoding = 'utf-8-sig') as f:
+        for c in data:
+            f.write(c + '\n')
+
+# 執行
+def main():
+    data = read_file('input.txt')
+    person = ['Allen', 'Tom']
+    chat = convert(data, person)
+    write_file('output.txt', chat)
+
+main()
