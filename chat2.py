b891c7260648ad18d171fd65dede1392f0f048c6
bellabula <zz225864@gmail.com>
2022-02-03 16:32:45 +0800

    chat2

--- chat2.py
+++ chat2.py
@@ -9,7 +9,7 @@ def read_file(filename):
     return data
 
 # 改寫對話紀錄形式
-def caht_split(data):
+def chat_split(data):
     chat = []
     allen_word_count = 0
     allen_sticker_count = 0
@@ -49,7 +49,7 @@ def write_file(filename, data):
 # 執行
 def main():
     data = read_file('LINE-Viki.txt')
-    chat, allen_word_count, allen_sticker_count, allen_image_count, viki_word_count, viki_sticker_count, viki_image_count = caht_split(data)
+    chat, allen_word_count, allen_sticker_count, allen_image_count, viki_word_count, viki_sticker_count, viki_image_count = chat_split(data)
     print('allen 說了', allen_word_count, '個字，傳了', allen_sticker_count, '個貼圖', allen_image_count, '張圖片')
     print('viki 說了', viki_word_count, '個字，傳了', viki_sticker_count, '個貼圖', viki_image_count, '張圖片')
     write_file('LINE-output.txt', chat)
